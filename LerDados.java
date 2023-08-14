55835e57c41283d523449cb73e4c5d8f17384a2c
Rafinhaas77 <49204121+Rafinhaas77@users.noreply.github.com>
2023-08-14 19:25:24 -0300

    Add files via upload

---
+++ LerDados.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class LerDados {
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(System.in));
+        //System.in: entrada padrão (teclado)
+        //InputStreamReader: captura um fluxo de entrada de dados
+        //BufferedReader: buffer de leitura
+                
+            String linha = "";
+            System.out.println("Digite alguma coisa ou S para sair:");
+            while (!linha.toUpperCase().equals("S")) {
+                linha = reader.readLine();
+                System.out.println("Digitou: " + linha);
+            }
+            //ou
+            // while (!linha.equals("S")) {
+            //     linha = reader.readLine();
+            //     System.out.println("Digitou: " + linha);
+            //     linha = linha.toUpperCase();
+            // }
+    }
+}
